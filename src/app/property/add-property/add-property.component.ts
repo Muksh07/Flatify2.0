c30bf3100c0b30fc4a559f27dc6473fc534d568b
Muksh07 <Mukesh91573@gmail.com>
2024-04-25 18:27:45 +0530

    Before Side Preview

--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -1,16 +1,23 @@
-import { NgIf } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { TabsModule, TabsetComponent } from 'ngx-bootstrap/tabs';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
 
 @Component({
   selector: 'app-add-property',
   standalone:true,
-  imports:[FormsModule,NgIf], 
+  imports:[FormsModule,NgIf,TabsModule,ButtonsModule,NgFor], 
   templateUrl: './add-property.component.html',
   styleUrls: ['./add-property.component.css']
 })
-export class AddPropertyComponent implements OnInit {
+export class AddPropertyComponent implements OnInit 
+{
+  propertytypes: Array<string> = ["House","Appartment","Duplex"];
+  furnishedtypes: Array<string> = ["Fully","Semi","Unfurnished"];
+  @ViewChild('tabset') tabset?: TabsetComponent
 
   constructor(private myrouter:Router) 
   { 
@@ -28,6 +35,12 @@ export class AddPropertyComponent implements OnInit {
   OnSubmit(myForm:any)
   {
     console.log(myForm);
+  }
+  nextTab(tabId:number)
+  {
+    if (this.tabset && this.tabset.tabs && tabId < this.tabset.tabs.length) {
+      this.tabset.tabs[tabId].active = true;
+    }
   }
 
 }
